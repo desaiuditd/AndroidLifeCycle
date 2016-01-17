5733f15ad16dde8e0f53fe58c099894176750ae6
Udit Desai <desaiuditd@gmail.com>
2016-01-17 15:05:01 -0800

    updates the super class method sequence

--- app/src/main/java/in/incognitech/androidlifecycle/MainActivity.java
+++ app/src/main/java/in/incognitech/androidlifecycle/MainActivity.java
@@ -8,50 +8,50 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
+        Toast.makeText( this.getApplicationContext(), getResources().getText( R.string.app_name ) + " created", Toast.LENGTH_LONG ).show();
         super.onCreate(savedInstanceState);
         // The activity is being created.
-        Toast.makeText( this.getApplicationContext(), getResources().getText( R.string.app_name ) + " created", Toast.LENGTH_LONG ).show();
     }
 
     @Override
     protected void onStart() {
+        Toast.makeText( this.getApplicationContext(), getResources().getText( R.string.app_name ) + " started", Toast.LENGTH_LONG ).show();
         super.onStart();
         // The activity is about to become visible.
-        Toast.makeText( this.getApplicationContext(), getResources().getText( R.string.app_name ) + " started", Toast.LENGTH_LONG ).show();
     }
 
     @Override
     protected void onResume() {
+        Toast.makeText( this.getApplicationContext(), getResources().getText( R.string.app_name ) + " resumed", Toast.LENGTH_LONG ).show();
         super.onResume();
         // The activity has become visible (it is now "resumed").
-        Toast.makeText( this.getApplicationContext(), getResources().getText( R.string.app_name ) + " resumed", Toast.LENGTH_LONG ).show();
     }
 
     @Override
     protected void onPause() {
+        Toast.makeText( this.getApplicationContext(), getResources().getText( R.string.app_name ) + " paused", Toast.LENGTH_LONG ).show();
         super.onPause();
         // Another activity is taking focus (this activity is about to be "paused").
-        Toast.makeText( this.getApplicationContext(), getResources().getText( R.string.app_name ) + " paused", Toast.LENGTH_LONG ).show();
     }
 
     @Override
     protected void onStop() {
+        Toast.makeText( this.getApplicationContext(), getResources().getText( R.string.app_name ) + " stopped", Toast.LENGTH_LONG ).show();
         super.onStop();
         // The activity is no longer visible (it is now "stopped")
-        Toast.makeText( this.getApplicationContext(), getResources().getText( R.string.app_name ) + " stopped", Toast.LENGTH_LONG ).show();
     }
 
     @Override
     protected void onRestart() {
-        super.onRestart();
         Toast.makeText( this.getApplicationContext(), getResources().getText( R.string.app_name ) + " restarted", Toast.LENGTH_LONG ).show();
+        super.onRestart();
     }
 
     @Override
     protected void onDestroy() {
+        Toast.makeText( this.getApplicationContext(), getResources().getText( R.string.app_name ) + " destroyed", Toast.LENGTH_LONG ).show();
         super.onDestroy();
         // The activity is about to be destroyed.
-        Toast.makeText( this.getApplicationContext(), getResources().getText( R.string.app_name ) + " destroyed", Toast.LENGTH_LONG ).show();
     }
 
 }
